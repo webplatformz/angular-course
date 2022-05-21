99444c06b3df3c4d7878846f4041e74c35b51139
Marco Romanutti <m.romanutti@gmail.com>
2022-05-21 08:39:47 +0200

    Access url parameter using observable

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { Observable, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-hero-detail',
@@ -12,25 +13,46 @@ import { HeroService } from '../hero.service';
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero | undefined;
+  nextId: number | undefined;
 
   constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) {}
 
   ngOnInit(): void {
-    this.getHero();
+    const currentHero$ = this.getHero();
+    this.setNextId(currentHero$);
   }
 
-  getHero(): void {
-    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
-    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  getHero(): Observable<Hero> {
+    /*     const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+    return this.heroService.getHero(id).pipe(tap(hero => (this.hero = hero))); */
+
+    return this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = parseInt(params.get('id')!, 10);
+        return this.heroService.getHero(id);
+      }),
+      tap(hero => (this.hero = hero))
+    );
   }
 
   goBack(): void {
     this.location.back();
+    console.log(this.hero);
   }
 
   save(): void {
     if (this.hero) {
       this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
     }
+  }
+
+  private setNextId(hero$: Observable<Hero>) {
+    hero$
+      .pipe(
+        switchMap(hero => {
+          return this.heroService.getHero(hero.id + 1);
+        })
+      )
+      .subscribe(next => (this.nextId = next?.id));
   }
 }

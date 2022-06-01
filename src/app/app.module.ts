5c8e2b0c7b8a26056a7324ab6ee956412ac78fca
Marco Romanutti <m.romanutti@gmail.com>
2022-06-01 18:14:01 +0200

    Add reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -18,7 +18,7 @@ import { MessagesComponent } from './messages/messages.component';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
 
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-detail',
@@ -12,10 +12,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
-  hero: Hero | undefined;
-
-  @ViewChild('heroForm')
-  heroForm: NgForm | undefined;
+  hero!: Hero;
+  heroForm!: FormGroup;
 
   constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) {}
 
@@ -25,16 +23,33 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
-    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+    this.heroService.getHero(id).subscribe(hero => {
+      this.hero = hero;
+      this.createForm(hero);
+    });
+  }
+
+  private createForm(hero: Hero) {
+    this.heroForm = new FormGroup({
+      name: new FormControl(hero.name, Validators.required),
+      nickname: new FormControl(hero.nickname),
+    });
+  }
+
+  isValid(): boolean {
+    return this.heroForm.valid;
   }
 
   goBack(): void {
     this.location.back();
-    this.heroForm?.reset();
+    this.heroForm.reset();
   }
 
   save(): void {
     if (this.hero) {
+      this.hero.name = this.heroForm.controls['name'].value;
+      this.hero.nickname = this.heroForm.controls['nickname'].value;
+
       this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
     }
   }

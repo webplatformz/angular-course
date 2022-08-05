01cc30406aebae5722b5f71e9da1f5e854c203e4
Lucas Schnüriger <lucas.schnueriger@me.com>
2022-08-05 11:53:29 +0200

    Add simple logging interceptor

--- src/app/interceptors/logging.interceptor.ts
+++ src/app/interceptors/logging.interceptor.ts
@@ -1,1 +1,30 @@
-export class LoggingInterceptor {}
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpEventType,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const start: number = Date.now();
+    return next.handle(request).pipe(
+      tap(response => {
+        if (response.type === HttpEventType.Response) {
+          this.logStats(request, response, start);
+        }
+      })
+    );
+  }
+
+  private logStats({ method, url }: HttpRequest<unknown>, { status, statusText }: HttpResponse<any>, start: number) {
+    console.log(
+      `${method} request to ${url} resulted in a ${statusText} (${status}) response after ${Date.now() - start} ms`
+    );
+  }
+}

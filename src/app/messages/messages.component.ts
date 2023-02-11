5bd4d3133d5a435692e14f234edf72f9373f2b1a
Marco Romanutti <m.romanutti@gmail.com>
2023-02-11 10:01:46 +0100

    Adapt change detection strategy temporarily

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesComponent {
   constructor(public messageService: MessageService) {}

ffdc14c27aacb0f5971731095ff468af8ce2745a
Silas Fueglistaler <silas.fueglistaler@vontobel.com>
2023-06-19 22:04:29 +0200

    Move dashboard to feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages/feature/messages.component';
 import { MessageBarComponent } from './messages/ui/message-bar/message-bar.component';
-import { DashboardComponent } from './dashboard/feature/dashboard.component';
-import { HeroesMenuComponent } from './dashboard/ui/heroes-menu/heroes-menu.component';
-import { HeroSearchComponent } from './dashboard/ui/hero-search/hero-search.component';
 import { HeroDetailModule } from './hero-detail/hero-detail.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, HeroDetailModule, HeroesModule],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    MessagesComponent,
-    HeroSearchComponent,
-    MessageBarComponent,
-    HeroesMenuComponent,
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HeroDetailModule,
+    HeroesModule,
+    DashboardModule,
   ],
+  declarations: [AppComponent, MessagesComponent, MessageBarComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

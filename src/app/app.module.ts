042754d4d36522028441291737a0ecf03ddc559e
Silas Fueglistaler <silas.fueglistaler@vontobel.com>
2023-06-19 22:10:18 +0200

    Move messages to feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { MessagesComponent } from './messages/feature/messages.component';
-import { MessageBarComponent } from './messages/ui/message-bar/message-bar.component';
 import { HeroDetailModule } from './hero-detail/hero-detail.module';
 import { HeroesModule } from './heroes/heroes.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { MessagesModule } from './messages/messages.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     HeroDetailModule,
     HeroesModule,
     DashboardModule,
+    MessagesModule,
   ],
-  declarations: [AppComponent, MessagesComponent, MessageBarComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

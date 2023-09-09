d6fd939bf73ed4888844614f1887856ff203a14f
Silas Fueglistaler <silas.fueglistaler@vontobel.com>
2023-09-09 23:30:16 +0200

    Refactor heroes.component to use signals

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, effect, OnInit, Signal, signal, WritableSignal } from '@angular/core';
 
 import { Classification, Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -9,19 +9,28 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
-  nameToFilter = '';
-  classificationToFilter: 'restricted' | 'public' | 'all' = 'all';
+  heroes: WritableSignal<Hero[]> = signal([]);
+  nameToFilter: WritableSignal<string> = signal('');
+  classificationToFilter: WritableSignal<'restricted' | 'public' | 'all'> = signal('all');
+
   CLASSIFIED = Classification.CLASSIFIED;
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) {
+    effect(() => {
+      console.log(
+        `calculating filteredHeroes, heroes:${
+          this.heroes().length
+        }, name:${this.nameToFilter()}, classification:${this.classificationToFilter()}`,
+      );
+    });
+  }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+    this.heroService.getHeroes().subscribe(heroes => this.heroes.set(heroes));
   }
 
   add(name: string): void {
@@ -30,41 +39,37 @@ export class HeroesComponent implements OnInit {
       return;
     }
     this.heroService.addHero({ name } as Hero).subscribe(hero => {
-      this.heroes.push(hero);
+      this.heroes.mutate(heroes => heroes.push(hero));
     });
   }
 
   delete(hero: Hero): void {
-    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroes.set(this.heroes().filter(h => h.id !== hero.id));
     this.heroService.deleteHero(hero.id).subscribe();
   }
 
-  get filteredHeroes(): Hero[] {
-    console.log(
-      `calculating filteredHeroes, heroes:${this.heroes.length}, name:${this.nameToFilter}, classification:${this.classificationToFilter}`,
-    );
-
-    return this.heroes
-      .filter(hero => hero.name.toLowerCase().includes(this.nameToFilter))
-      .filter(hero => {
-        if (this.classificationToFilter === 'all') {
+  filteredHeroes: Signal<Hero[]> = computed(() =>
+    this.heroes()
+      .filter(h => h.name.toLowerCase().includes(this.nameToFilter()))
+      .filter(h => {
+        if (this.classificationToFilter() === 'all') {
           return true;
         }
-        if (this.classificationToFilter === 'public') {
-          return hero.classification === Classification.PUBLIC;
+        if (this.classificationToFilter() === 'public') {
+          return h.classification === Classification.PUBLIC;
         }
-        if (this.classificationToFilter === 'restricted') {
-          return hero.classification === Classification.CLASSIFIED;
+        if (this.classificationToFilter() === 'restricted') {
+          return h.classification === Classification.CLASSIFIED;
         }
         return false;
-      });
-  }
+      }),
+  );
 
   nameFilterChanged(filterValue: string) {
-    this.nameToFilter = filterValue;
+    this.nameToFilter.set(filterValue);
   }
 
   classificationFilterChanged(filterValue: 'restricted' | 'public' | 'all'): void {
-    this.classificationToFilter = filterValue;
+    this.classificationToFilter.set(filterValue);
   }
 }

92a001b0ab8992a8846d8ec37f75d6f83f9da5e9
Silas Fueglistaler <silas.fueglistaler@zuehlke.com>
2023-11-11 16:35:13 +0100

    Change to reactive form

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { LoggingService } from '../logging-service';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-detail',
@@ -14,15 +14,16 @@ import { NgForm } from '@angular/forms';
 })
 export class HeroDetailComponent implements OnInit {
   hero!: Hero;
-
-  @ViewChild('heroForm')
-  heroForm!: NgForm;
+  heroForm = new FormGroup({
+    name: new FormControl<string>('', Validators.required),
+    description: new FormControl<string>(''),
+  });
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private loggingService: LoggingService,
-    private location: Location
+    private location: Location,
   ) {}
 
   ngOnInit(): void {
@@ -31,17 +32,33 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
-    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+    this.heroService.getHero(id).subscribe(hero => {
+      this.hero = hero;
+      this.updateForm(hero);
+    });
     this.loggingService.log(`Selected heroes id is ${id}`);
+  }
+
+  private updateForm(hero: Hero) {
+    this.heroForm.patchValue({
+      name: hero.name,
+      description: hero.description,
+    });
+  }
+
+  isValid(): boolean {
+    return this.heroForm.valid;
   }
 
   goBack(): void {
     this.location.back();
-    this.heroForm?.reset();
+    this.heroForm.reset();
   }
 
   save(): void {
     if (this.hero) {
+      this.hero.name = this.heroForm.controls.name.value!;
+      this.hero.description = this.heroForm.controls.description.value!;
       this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
     }
   }

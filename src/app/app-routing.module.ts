d6617e8b4478f50a7d304177b42ce0fae4ba4710
Silas Fueglistaler <silas.fueglistaler@zuehlke.com>
2023-11-13 12:59:45 +0100

    Add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroClassificationGuard } from './hero-classification.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'detail/:id', component: HeroDetailComponent, canActivate: [HeroClassificationGuard] },
   { path: 'heroes', component: HeroesComponent },
 ];
 

3fd7daab7ba5498011e188a8a5f465a9d6fd9c8d
Silas Fueglistaler <silas.fueglistaler@zuehlke.com>
2023-11-19 16:09:15 +0100

    Add space pipe

--- src/app/spaced.pipe.ts
+++ src/app/spaced.pipe.ts
@@ -4,5 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'spaced',
 })
 export class SpacedPipe implements PipeTransform {
-  transform(value: any, ...args: any[]): any {}
+  transform(value: string): any {
+    return value.split('').join(' ');
+  }
 }

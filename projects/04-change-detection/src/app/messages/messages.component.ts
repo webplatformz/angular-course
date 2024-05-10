af26a5729f9f977bccb1f5b6e510a08aa2b29edd
sifu <silasfue@gmail.com>
2024-05-10 13:35:50 +0200

    exercise 04-change-detection

---
+++ projects/04-change-detection/src/app/messages/messages.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+})
+export class MessagesComponent {
+  constructor(public messageService: MessageService) {}
+}

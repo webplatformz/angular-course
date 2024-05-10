34ab8b3a98a5f9b53205b801bf47fba292c11277
sifu <silasfue@gmail.com>
2024-05-10 12:59:37 +0200

    exercise and solution 03-reactive-forms and 03-reactive-forms_solution

---
+++ projects/03-reactive-forms/src/main.ts
@@ -0,0 +1,20 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app/app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, FormsModule, AppRoutingModule),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+}).catch(err => console.error(err));

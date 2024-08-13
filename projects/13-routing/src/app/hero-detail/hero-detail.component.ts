20b7ef6e7bb72db66ed5408b4a01cd30de71f6c5
Marco Romanutti <m.romanutti@gmail.com>
2024-08-13 14:55:37 +0200

    Add exercises for backend requests and routing

---
+++ projects/13-routing/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location, UpperCasePipe } from '@angular/common';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css'],
+  standalone: true,
+  imports: [FormsModule, UpperCasePipe],
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location,
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
+  }
+}

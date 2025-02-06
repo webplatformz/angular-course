02cdd3ee33cc660eab13b0df383801e5de092127
phim <philipp.imperatori@zuehlke.com>
2025-02-06 12:00:41 +0100

    defer-section: add e2e tests

--- projects/14-defer_solution/cypress/e2e/app.cy.ts
+++ projects/14-defer_solution/cypress/e2e/app.cy.ts
@@ -1,4 +1,4 @@
-describe('Tour of Heroes', () => {
+describe('Defer Section', () => {
   beforeEach(() => {
     cy.readFile('../../api/db.json').then(db => {
       cy.writeFile('../../api/db.json.bak', db);
@@ -15,84 +15,67 @@ describe('Tour of Heroes', () => {
     cy.visit('http://localhost:4200');
   });
 
-  it('Initial page', () => {
-    // has title 'Tour of Heroes'
-    cy.title().should('contain', 'Tour of Heroes');
-    // has h1 'Tour of Heroes'
-    cy.get('h1').should('have.text', 'Tour of Heroes');
-    // has views 'Dashboard' and 'Heroes'
-    cy.get('nav').contains('a', 'Dashboard');
-    cy.get('nav').contains('a', 'Heroes');
-    // has dashboard as the active view
-    cy.get('app-dashboard').should('be.visible');
+  it('Viewport Tests', () => {
+    cy.visit('http://localhost:4200/viewport');
+
+    // Ensure the initial state before scrolling
+    cy.get('app-stopwatch').should('have.length', 1);
+    cy.get('.placeholder').should('be.visible');
+
+    // Scroll to the bottom
+    cy.scrollTo('bottom');
+
+    // Ensure the second stopwatch is loaded
+    cy.get('app-stopwatch').should('have.length', 2);
+
+    // Assert that the placeholder is no longer in the DOM
+    cy.get('.placeholder').should('not.exist');
   });
 
-  it('Dashboard Tests', () => {
-    // displays messages
-    cy.get('app-messages').contains('fetched heroes');
-    // has top heroes
-    cy.get('.heroes-menu > a').should('have.length', 4);
-    // selects and routes to hero details
-    cy.contains('Magneta').click();
-    cy.get('h2').should('contain.text', 'MAGNETA Details');
-    // updates hero name in details view
-    cy.get('#hero-name').should('have.value', 'Magneta');
-    cy.get('#hero-name').type('X');
-    cy.get('h2').should('contain.text', 'MAGNETAX Details');
-    // cancels and shows hero in dashboard
-    cy.contains('go back').click();
-    cy.contains('magnetax', { matchCase: false }).should('not.exist');
-    // selects and routes to hero details
-    cy.get('.heroes-menu').within(() => {
-      cy.contains('Magneta').click();
-    });
-    // updates hero name in details view
-    cy.get('#hero-name').should('have.value', 'Magneta').type('Y');
-    // saves and shows hero in Dashboard
-    cy.contains('save').click();
-    cy.get('.heroes-menu').within(() => {
-      cy.contains('magnetay', { matchCase: false });
-    });
+  it('Interaction Tests', () => {
+    cy.visit('http://localhost:4200/interaction');
+
+    // Ensure the initial state before scrolling
+    cy.get('app-stopwatch').should('have.length', 1);
+    cy.get('.placeholder').should('be.visible');
+
+    // Click the interaction button
+    cy.get('button')
+      .should('be.visible') // Ensure it's visible
+      .should('not.be.disabled') // Ensure it's clickable
+      .click();
+
+    // Ensure the second stopwatch is loaded
+    cy.get('app-stopwatch').should('have.length', 2);
+
+    // Assert that the placeholder is no longer in the DOM
+    cy.get('.placeholder').should('not.exist');
   });
 
-  it('Heroes tests', () => {
-    // can switch to Heroes view
-    cy.get('nav').contains('a', 'Heroes').click();
-    cy.get('app-heroes').should('be.visible');
-    cy.get('.heroes > li').should('have.length', 10);
-    // can route to hero details
-    cy.get('.heroes > li').contains('Magneta').click();
-    // updates hero name in details view
-    cy.get('#hero-name').should('have.value', 'Magneta').type('Z');
-    cy.contains('save').click();
-    // shows updated hero in Heroes list
-    cy.get('.heroes > li').contains('MagnetaZ');
-    // deletes hero from Heroes list
-    cy.get('.heroes > li').contains('MagnetaZ').siblings('button.delete').click();
-    cy.get('.heroes > li').should('have.length', 9);
-    // adds back hero
-    cy.get('#new-hero').type('Alice');
-    cy.contains('add hero', { matchCase: false }).click();
-    cy.get('.heroes > li').contains('Alice');
-    cy.get('.heroes > li').should('have.length', 10);
-    // displays correctly styled buttons
-    cy.contains('button', 'x')
-      .should('have.css', 'font-family', 'Arial, Helvetica, sans-serif')
-      .and('have.css', 'border-top-style', 'none')
-      .and('have.css', 'padding', '1px 10px 3px')
-      .and('have.css', 'border-radius', '4px')
-      .and('have.css', 'left', '210px')
-      .and('have.css', 'top', '5px');
-    cy.contains('button', 'add hero', { matchCase: false })
-      .should('have.css', 'font-family', 'Arial, Helvetica, sans-serif')
-      .and('have.css', 'border-top-style', 'none')
-      .and('have.css', 'padding', '8px 24px')
-      .and('have.css', 'border-radius', '4px');
-  });
+  it('Loading Tests', () => {
+    cy.visit('http://localhost:4200/loading');
 
-  it('Hero search', () => {
-    cy.get('#search-box').should('be.visible').type('Magneta');
-    cy.get('.search-result > li').should('have.length', 1).and('contain.text', 'Magneta').click();
-    cy.get('app-hero-detail').should('be.visible');
+    // Ensure the initial state before scrolling
+    cy.get('app-stopwatch').should('have.length', 1);
+    cy.get('.placeholder').should('be.visible');
+
+    // Click the interaction button
+    cy.get('button')
+      .should('be.visible') // Ensure it's visible
+      .should('not.be.disabled') // Ensure it's clickable
+      .click();
+
+    // Ensure "Is loading" text appears
+    cy.get('p').contains('loading').should('be.visible');
+
+    // Wait for the minimum loading time (5 seconds)
+    cy.wait(5000);
+
+    // Ensure the second stopwatch is loaded
+    cy.get('app-stopwatch').should('have.length', 2);
+
+    // Assert that the placeholder & loading msg is no longer in the DOM
+    cy.get('.placeholder').should('not.exist');
+    cy.get('p').contains('🔄 Is loading for at least 5 secs...').should('not.exist');
   });
 });

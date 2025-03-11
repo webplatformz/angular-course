7fb511d7feb316b3df34e0f41c4fd9b06d103078
sifu <silasfue@gmail.com>
2025-03-11 10:35:36 +0100

    update signals exercise and solution

--- projects/11-signals/src/app/message.service.ts
+++ projects/11-signals/src/app/message.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Signal, signal, WritableSignal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class MessageService {
-  private _messages: WritableSignal<string[]> = signal([]);
+  private _messages$: WritableSignal<string[]> = signal([]);
 
   add(message: string) {
     // todo: add message to _messages, hint: use 'update' method
@@ -12,7 +12,7 @@ export class MessageService {
     // todo: implement, hint: use 'set' method
   }
 
-  get messages(): Signal<string[]> {
-    return this._messages.asReadonly();
+  get messages$(): Signal<string[]> {
+    return this._messages$.asReadonly();
   }
 }
